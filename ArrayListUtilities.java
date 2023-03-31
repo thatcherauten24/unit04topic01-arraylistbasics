3192ab6d645b7d1c2b42aa44e37a443712da0081
Thatcher Auten <thatcherauten24@gmail.com>
2023-03-31 11:49:19 -0500

    working on reverseInPlace

--- ArrayListUtilities.java
+++ ArrayListUtilities.java
@@ -15,9 +15,9 @@ public class ArrayListUtilities {
     public static ArrayList<String> withoutLongWords(ArrayList<String> withLongWords) {
         ArrayList<String> result = withLongWords;
 
-        for (int i = withLongWords.size() - 1; i > 0; i--) {
-            if (withLongWords.get(i).length() > 5) {
-                withLongWords.remove(i);
+        for (int i = result.size() - 1; i > 0; i--) {
+            if (result.get(i).length() > 5) {
+                result.remove(i);
             }
         }
 
@@ -25,7 +25,28 @@ public class ArrayListUtilities {
     }
 
     public static void removeLongWords(ArrayList<String> withLongWords) {
+        for (int i = withLongWords.size() - 1; i > 0; i--) {
+            if (withLongWords.get(i).length() > 5) {
+                withLongWords.remove(i);
+            }
+        }
+    }
 
+    public static ArrayList<Double> reversed(ArrayList<Double> nums) {
+        ArrayList<Double> reversedNums = new ArrayList<Double>();
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            reversedNums.add(nums.get(i));
+        }
+        return reversedNums;
+    }
+
+    public static void reverseInPlace(ArrayList<Double> nums) {
+        for (int fromEnd = nums.size() - 1; fromEnd >= 0; fromEnd--) {    
+            for (int fromStart = 0; fromStart < nums.size(); fromStart++) {
+                nums.add(fromStart, nums.get(fromEnd));
+            }
+        }
     }
 
     public static void main(String[] args) {
--- ArrayListUtilitiesTest.java
+++ ArrayListUtilitiesTest.java
@@ -21,14 +21,54 @@ public class ArrayListUtilitiesTest {
 
     @Test
     public void testWithoutLongWords() {
-        ArrayList<String>input;
+        ArrayList<String> input;
         ArrayList<String> expectedOutput;
         ArrayList<String> actualOutput;
 
         // some positive numbers
         input = new ArrayList<>(Arrays.asList("rat", "paper", "computer", "bomb", "mouse", "keyboard"));
-        expectedOutput = new ArrayList<String> ( Arrays.asList("rat", "paper", "bomb", "mouse") );
+        expectedOutput = new ArrayList<String> (Arrays.asList("rat", "paper", "bomb", "mouse"));
         actualOutput = ArrayListUtilities.withoutLongWords(input);
         assertEquals(expectedOutput, actualOutput);
     }
+
+    @Test
+    public void testRemoveLongWords() {
+        ArrayList<String> original;
+        ArrayList<String> expectedFinal;
+
+        // some positive numbers
+        original = new ArrayList<>(Arrays.asList("rat", "paper", "computer", "bomb", "mouse", "keyboard"));
+        expectedFinal = new ArrayList<String> (Arrays.asList("rat", "paper", "bomb", "mouse"));
+        ArrayListUtilities.removeLongWords(original);
+        assertEquals(expectedFinal, original);
+    }
+
+    @Test
+    public void testReversed() {
+        ArrayList<Double> input;
+        ArrayList<Double> expectedOutput;
+        ArrayList<Double> actualOutput;
+
+        // some positive numbers
+        input = new ArrayList<>(Arrays.asList(12.0, 3.5, 8.9, 6.1));
+        expectedOutput = new ArrayList<Double> (Arrays.asList(6.1, 8.9, 3.5, 12.0));
+        actualOutput = ArrayListUtilities.reversed(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testreverseInPlace() {
+        ArrayList<Double> original;
+        ArrayList<Double> expectedFinal;
+
+        // some positive numbers
+        original = new ArrayList<>(Arrays.asList(12.0, 3.5, 8.9, 6.1));
+        expectedFinal = new ArrayList<Double> (Arrays.asList(6.1, 8.9, 3.5, 12.0));
+        ArrayListUtilities.reverseInPlace(original);
+        assertEquals(expectedFinal, original);
+    }
+
+    
+
 }
